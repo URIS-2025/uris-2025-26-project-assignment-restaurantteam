4c133781efa47ab725cce7e9e07f35be7cac002d
Ethereallie13 <pavle.rakic2002@gmail.com>
2026-04-18 23:26:45 +0200

    added postman export for all services

--- AuthenticationService/Entities/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthenticationService.Entities
-{
-    public class LoginRequest
-    {
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-    }
-}
